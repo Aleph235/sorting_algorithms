372d7483735169b878bd10eb24a2b0a5ffbe1968
Aleph235 <akram239911@gmail.com>
2022-06-09 22:06:05 +0200

    buble-sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+#include <stdbool.h>
+
+/**
+ * swap - swaps two integers
+ * @xp: pointer to int x
+ * @yp: pointer to int y
+ * Return: void
+ */
+void swap(int *xp, int *yp)
+{
+	int tmp = *xp;
+	*xp = *yp;
+	*yp = tmp;
+}
+/**
+ * bubble_sort - sort array with buble algorithm
+ * @array: given array
+ * @size: array's size
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	bool swapped;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = false;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				swapped = true;
+				print_array(array, size);
+			}
+		}
+
+		if (size == 1 || swapped == false)
+			break;
+	}
+}
