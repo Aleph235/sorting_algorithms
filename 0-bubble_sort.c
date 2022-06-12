7e0be0905e3e72e050689c766baa2ca2e83565b6
khawladaouay <daouaykhawla782@gmail.com>
2022-06-12 20:54:33 +0100

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,16 +8,16 @@
 void bubble_sort(int *array, size_t size)
 {
 size_t i, j;
-int aux;
+int swp;
 for (i = 0; i < size; i++)
 {
-	for (j = 0; j < size - i - 1; j++)
+	for (j = 0; j < size - 1 - i; j++)
 	{
 		if (array[j] > array[j + 1])
 		{
-			aux = array[j];
+			swp = array[j];
 			array[j] = array[j + 1];
-			array[j + 1] = aux;
+			array[j + 1] = swp;
 		}
 	}
 	print_array(array, size);
