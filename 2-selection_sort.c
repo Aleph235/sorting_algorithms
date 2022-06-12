c94b9c49eb7f2c1eff2f13b658bba9ecaae4e6da
khawladaouay <daouaykhawla782@gmail.com>
2022-06-12 21:54:36 +0100

    task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,20 +8,20 @@
  */
 void selection_sort(int *array, size_t size)
 {
-for (int i = 0; i < size - 1; i++)
-{
-	int min_pos = i;
-
-	for (int j = i + 1; j < size; j++)
-		if (array[j] < array[min_pos])
-			min_pos = j;
-	if (min_pos != i)
+	for (int i = 0; i < size - 1; i++)
 	{
-		int temp = array[i];
-
-		array[i] = array[min_pos];
-		array[min_pos] = temp;
+		int min_pos = i;
+	
+		for (int j = i + 1; j < size; j++)
+			if (array[j] < array[min_pos])
+				min_pos = j;
+		if (min_pos != i)
+		{
+			int temp = array[i];
+	
+			array[i] = array[min_pos];
+			array[min_pos] = temp;
+		}
+		print_array(array, size);
 	}
-	print_array(array, size);
-}
 }
