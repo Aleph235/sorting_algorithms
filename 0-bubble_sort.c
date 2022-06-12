34bec007a7219e814547b24cdc6ae2b75e55e016
Aleph235 <akram239911@gmail.com>
2022-06-12 21:35:05 +0200

    first fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-#include <stdbool.h>
 
 /**
  * swap - swaps two integers
@@ -22,22 +21,19 @@ void swap(int *xp, int *yp)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	bool swapped;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = false;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				swap(&array[j], &array[j + 1]);
-				swapped = true;
 				print_array(array, size);
 			}
 		}
 
-		if (size < 2 || swapped == false)
+		if (size < 2)
 			break;
 	}
 }
